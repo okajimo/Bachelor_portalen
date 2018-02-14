e2c83981277b16c2dab909fbf73c07f9f1f0ff76
okajimo <s305516@stud.hioa.no>
2018-02-14 17:55:35 +0100

    Bootstrap 4 og alt fungerer

--- BPO/resources/views/inc/navbar.blade.php
+++ BPO/resources/views/inc/navbar.blade.php
@@ -1,31 +1,29 @@
-<!--<nav class="navbar navbar-inverse">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <!--<a class="navbar-brand" href="/">{{config('app.name', 'BPO')}}</a>--><!--
-            <a class="navbar-brand" href="/"><img src="https://student.hioa.no/hioa-theme/images/hioa-logo-no.svg" alt="Student" height="75" width="80"></a>
-        </div>
-        <div>
-            <section class="info">Bachelorprosjekt i informasjonsteknologi</section>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <!-- Venstre side av navbar -->
-            <ul class="nav navbar-nav navbar-left">
-                <li><a href="/informasjon">Informasjon</a></li>
-                <li><a href="/prosjektforslag">Prosjektforslag</a></li>
-                <li><a href="/tidligere_prosjekter">Tidligere Prosjekter</a></li>
-                <li><a href="/grupper">Grupper</a></li>
-                <li><a href="/kontakt_info">Kontakt oss</a></li>
-            </ul>
-        </div><!--/.nav-collapse --><!--
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/"><img src="https://student.hioa.no/hioa-theme/images/hioa-logo-no.svg" alt="Student" height="75" width="80"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-//stian og martin 
-            <!-- Høyre side av navbar -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/informasjon">Informasjon <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/prosjektforslag">Prosjektforslag</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/tidligere_prosjekter">Tidligere Prosjekter</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/grupper">Grupper</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/kontakt_info">Kontakt oss</a>
+            </li>
+            
+            </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Logg inn</a></li>
@@ -39,7 +37,7 @@
                             <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                                                document.getElementById('logout-form').submit();">
                                     Logout
                                 </a>
 
@@ -52,30 +50,5 @@
                 @endif
             <ul>
         </div>
-    </div>
-</nav>-->
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+    </nav>
+</div>
