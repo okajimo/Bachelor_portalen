b9fbc8d542051a7854922451b04922b742b2face
Martin <s305454@stud.hioa.no>
2018-02-21 23:37:17 +0100

    Lagt til error meldinger i app.blade.php som vises på alle sidene.

--- BPO/resources/views/layouts/app.blade.php
+++ BPO/resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 <body>
     @include('inc.navbar')
     @include('inc.nav_header')
+    @include('inc.feilmld') <!-- Legger ut feilmeldinger på siden, eks: prøver å laste opp dokument, men manger fil -->
     <div class="container">
         <h3 style="border-bottom: 2px solid #ffe01d; float: left; padding-bottom: 5px">{{$title}}</h3>
         @yield('content')
