ee20ad27eb331c4608d7eee5080e1d2b2766d2d7
Ivo <s305516@stud.hioa.no>
2018-02-24 21:50:23 +0100

    Gjorde en siste liten tweek på navigasjonsbarene på info ->student vil ha grønnt lys på å implementere denne stilen på alle de andre sidene.

--- BPO/resources/views/inc/nav_side.blade.php
+++ BPO/resources/views/inc/nav_side.blade.php
@@ -2,15 +2,15 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-12 col-xl-12">
-                <a class="nav-link" href="{{ asset('files/Statusrapport.pdf')}}"><h4>Statusrapport</h4></a>
-                <a class="nav-link" href="/dokumentasjon"><h4>Dokumetasjon</h4></a>
-                <a class="nav-link" href="{{ asset('files/Prosjektskisse.pdf')}}"><h4>Prosjektskisse</h4></a>
-                <a class="nav-link" href="/veiledning"><h4>Veiledning</h4></a>
+                <a class="nav-link nav-link-padding" href="{{ asset('files/Statusrapport.pdf')}}"><h4>Statusrapport</h4></a>
+                <a class="nav-link nav-link-padding" href="/dokumentasjon"><h4>Dokumetasjon</h4></a>
+                <a class="nav-link nav-link-padding" href="{{ asset('files/Prosjektskisse.pdf')}}"><h4>Prosjektskisse</h4></a>
+                <a class="nav-link nav-link-padding" href="/veiledning"><h4>Veiledning</h4></a>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-12 col-xl-12">
-                <a class="nav-link" href="/forprosjekt"><h4>Forprosjekt</h4></a>
-                <a class="nav-link" href="/prosjektrapport"><h4>Prosjektrapport</h4></a>
-                <a class="nav-link" href="/evaluering"><h4>Evaluering</h4></a>
+                <a class="nav-link nav-link-padding" href="/forprosjekt"><h4>Forprosjekt</h4></a>
+                <a class="nav-link nav-link-padding" href="/prosjektrapport"><h4>Prosjektrapport</h4></a>
+                <a class="nav-link nav-link-padding" href="/evaluering"><h4>Evaluering</h4></a>
             </div>
         </div>
     </div>  
