c725d7bf83595e924f611e166144dcd41ae29c3f
Stian <stian__95@hotmail.com>
2018-02-27 18:56:05 +0100

    Endret design på tablelen hvor gruppene skrives ut og knappene.

--- BPO/resources/views/pages/gruppe/vgruppe.blade.php
+++ BPO/resources/views/pages/gruppe/vgruppe.blade.php
@@ -8,14 +8,19 @@
                 $iGruppe = DB::select('SELECT student_groups.student FROM student_groups WHERE student_groups.student = :iGruppe', ['iGruppe' => $studentIGruppe]);
             ?>
             @if($iGruppe == null)
+            <div class="btn-group btn-group-lg" role="group" aria-label="Basic example">
                 {!! Form::open(['action' => 'GruppeController@lag_gruppe', 'method' => 'POST'])!!}
-                    {{Form::submit('Registrer gruppe', ['class'=>'btn btn-primary'])}}
+                    {{Form::submit('Registrer gruppe', ['class'=>'btn btn-success'])}}
                 {!! Form::close() !!}
-            </br>
+            </div>
+            
             @endif
             <?php 
                 $brukerS = DB::select('SELECT student_groups.student FROM student_groups WHERE student_groups.student LIKE :bruker',['bruker' => $bruker]);
             ?>
+            <!--prøv denne-->
+            
+
             @foreach($brukerS as $brukerF)
                 @if($brukerF->student == $bruker)
                 <?php $break = 0; ?>
@@ -28,24 +33,27 @@
                 @endforeach
 
                 @if($break == 0)
+                <div class="btn-group btn-group-lg" role="group" aria-label="Basic example">
                     {!! Form::open(['action' => 'GruppeController@sett_leder', 'method' => 'POST'])!!}
                         {{form::hidden('_method', 'PUT')}}
                         {{Form::submit('Sett leder', ['class'=>'btn btn-primary'])}}
                     {!! Form::close() !!}
-                    </br>
+                </div>
                 @endif
 
             @endif
             @endforeach
             @if($iGruppe ==! null)
+            <div class="btn-group btn-group-lg" role="group" aria-label="Basic example">
                 {!! Form::open(['action' => 'GruppeController@fjern_student', 'method' => 'POST'])!!}
                 {{form::hidden('_method', 'DELETE')}}
                     {{Form::submit('Forlat gruppe', ['class'=>'btn btn-danger'])}}
                 {!! Form::close() !!}
-                </br>
+            </div>
                 @endif
-            <table class="table">
-                    <tr style="border: solid;">
+            <table class="table"> <!-- Ivo fiks mobil vennlig. -->
+                    <thead class="thead-dark">
+                    <tr>
                         <th>Gruppe</th>
                         <th>Prosjektside</th>
                         <th>Studenter</th>
@@ -55,15 +63,17 @@
                         <th>Meld deg inn</th>
                         @endif
                     </tr>
+                    </thead>
                         @foreach ($groups as $group)
                         @if($group->leader ==! "")
                             <?php $date = date('Y'); ?>
                             @if($group->year >= date('Y'))
-                                <tr>
-    <!-- Lagt til av stian, Ivo fiks css -->   <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid; border-left: thin #cc3300 solid;">{{ $group->group_number }}
+                            <tbody>
+                            <tr>
+    <!-- Lagt til av stian, Ivo fiks css -->   <td>{{ $group->group_number }}
                                                </td>
-    <!-- Lagt til av stian, Ivo fiks css -->   <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid;"><a href="http://{{ $group->url }}" >{{ $group->title }}</a></td>
-    <!-- Lagt til av stian, Ivo fiks css -->   <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid;">
+    <!-- Lagt til av stian, Ivo fiks css -->   <td><a href="http://{{ $group->url }}" >{{ $group->title }}</a></td>
+    <!-- Lagt til av stian, Ivo fiks css -->   <td>
 
                                         <?php $student = DB::select('select student_groups.student from student_groups, groups 
                                         where groups.group_number = student_groups.student_groups_number and groups.year = student_groups_year 
@@ -82,7 +92,7 @@
                                             @endif
                                         @endforeach
                                     </td>
-    <!-- Lagt til av stian, Ivo fiks css -->   <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid;">
+    <!-- Lagt til av stian, Ivo fiks css -->   <td>
                                         <?php $email = DB::select('select users.email from users, student, student_groups, groups where users.username = student.username 
                                         and student.username = student_groups.student and student_groups.student_groups_number = groups.group_number 
                                         AND student_groups.student_groups_year = groups.year AND student_groups.student_groups_number LIKE :number 
@@ -93,18 +103,19 @@
 
                                         @endforeach
                                     </td>
-    <!-- Lagt til av stian, Ivo fiks css -->   <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid;">{{ $group->supervisor }}</td>
+    <!-- Lagt til av stian, Ivo fiks css -->   <td >{{ $group->supervisor }}</td>
                                                  @if($iGruppe == null)
-                                                    <td style="border-bottom: thin #cc3300 solid; border-right: thin #cc3300 solid; text-align: center;">
+                                                    <td style=" text-align: center;">
                                                         {!! Form::open(['action' => 'GruppeController@meld_inn', 'method' => 'POST'])!!}
                                                         {{Form::hidden('number', $group->group_number)}}
                                                         {{Form::hidden('year', $group->year)}}
                                                         {{form::hidden('_method', 'options')}}
-                                                        {{Form::submit('Bli med',['class'=>'btn btn-success', 'name' => 'meld'])}}
+                                                        {{Form::submit('Bli med',['class'=>'btn btn-success btn-nice', 'name' => 'meld'])}}
                                                         {!! Form::close() !!}
                                                     </td>
                                                 @endif
                                 </tr> 
+                                </tbody>
                             @endif
                         
                         @endif
