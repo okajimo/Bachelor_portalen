0f356313efba0643cca199246859b3c6678024d9
Martin <s305454@stud.hioa.no>
2018-02-28 18:15:13 +0100

    Lagt til visning av filer på sidene for opplasting. Viser kun det som opplastet for innlogget student (gruppe)

--- BPO/resources/views/pages/gruppe/lastOppSkisse.blade.php
+++ BPO/resources/views/pages/gruppe/lastOppSkisse.blade.php
@@ -13,5 +13,29 @@
                 {{Form::submit('Last opp dokument', ['class'=>'btn btn-primary'])}}    
             {!! Form::close() !!}
         </div>
+        <div>
+        </br>
+            <?php 
+                $student = session('navn');
+                $group = DB::select('SELECT groups.group_number, groups.year FROM student_groups, groups WHERE groups.group_number = student_groups.student_groups_number AND 
+                groups.year = student_groups.student_groups_year AND student_groups.student LIKE :student', ['student' => $student]);
+                $documents = DB::select('SELECT * FROM documents WHERE documents_groups_number LIKE :groupNumber AND documents_year LIKE :year AND title LIKE :type', 
+                ['groupNumber' => $group[0]->group_number, 'year' => $group[0]->year, 'type' => 'prosjektskisse']);
+            ?>
+            @if ($documents)
+                <table class="table table-responsive">
+                    <tr>
+                        <td>Dokument</td>
+                        <td>Type</td>
+                    </tr>
+                    @foreach ($documents as $doc)
+                        <tr>
+                            <td><a href="{{ asset('storage/filer/prosjektskisser/'.$doc->file_name)}}">{{ $doc->file_name}}</a></td>
+                            <td>prosjektskisse</td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endif
+        </div>
     </div>
 @endsection
