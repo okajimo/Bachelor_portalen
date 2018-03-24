458073774f6dfe53553209e3f911334f2b265d3b
Ivo <s305516@stud.hioa.no>
2018-03-24 06:06:10 +0100

    gjort hoved navbar mobilvennlig

--- BPO/resources/views/inc/navbar.blade.php
+++ BPO/resources/views/inc/navbar.blade.php
@@ -1,32 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom:1px solid #bbbdbf;"><!-- Lagt til av Stian, fiks css Ivo!  -->
-    <div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding:0; border-bottom:1px solid #bbbdbf;"><!-- Lagt til av Stian, fiks css Ivo!  -->
+    <div class="container" style="padding: 0 1em">
         <a class="navbar-brand" href="/"><img src="https://student.hioa.no/hioa-theme/images/hioa-logo-no.svg" alt="Student" height="75" width="80"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/informasjon">Informasjon <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/prosjektforslag">Prosjektforslag</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/tidligere_prosjekter">Tidligere Prosjekter</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/grupper">Grupper</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/kontakt_info">Kontakt oss</a>
-                </li>
-            </ul>
+        <div class="collapse navbar-collapse">
+            @include('inc/navbar_innhold')
         </div>
         <div class="nav">
             @include('inc/login')
         </div>
     </div>
 </nav>
-
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class=" d-lg-none navbar-light">
+        <div class="container hover-fix">
+            @include('inc/navbar_innhold')
+        </div>
+    </div>
+</div>
---
+++ BPO/resources/views/inc/navbar_innhold.blade.php
@@ -0,0 +1,17 @@
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="/informasjon">Informasjon <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/prosjektforslag">Prosjektforslag</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/tidligere_prosjekter">Tidligere Prosjekter</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/grupper">Grupper</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/kontakt_info">Kontakt oss</a>
+    </li>
+</ul>
