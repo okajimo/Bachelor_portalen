5eb82905bcef954e3eacd03b066162716eb90a31
Stian <stian__95@hotmail.com>
2018-04-02 23:08:05 +0200

    Legg til bulletpoints på linkene under dokumentasjon, det bør også stå litt forklaring i tekst form på siden for å hjelpe å fremheve meningen, linkene må også ha en annen farge og skriften må være mindre UU bootstrap den riktig

--- BPO/resources/views/pages/student/dokumentasjon.blade.php
+++ BPO/resources/views/pages/student/dokumentasjon.blade.php
@@ -2,9 +2,13 @@
 @section('content')
     <div class="jumbotron">
         <div class="container">
-            <a target='_blank' href="{{ asset('files/dokumentasjonsstandard-2.pdf')}}"><h4>Dokumentasjonsforslag for bachelorprosjekter (kun veiledende)</h4></a>
-            <a href="/lastNed/Avtalemal.docx"><h4>Avtalemal mellom HiOA, studenter og oppdragsgiver</h4></a>
-            <a href="/lastNed/Tittelside.docx"><h4>Tittelside for bachelorprosjekter</h4></a>
+            <h2>Nødvendige dokumenter for bacheloroppgave</h2>
+                </br>
+            <i class="fa fa-dot-circle-o" aria-hidden="true" style="color:black;"></i><a target='_blank' href="{{ asset('files/dokumentasjonsstandard-2.pdf')}}"> Dokumentasjonsforslag for bachelorprosjekter (kun veiledende) <i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                </br>
+            <i class="fa fa-dot-circle-o" aria-hidden="true" style="color:black;"></i><a href="/lastNed/Avtalemal.docx"> Avtalemal mellom HiOA, studenter og oppdragsgiver <i class="fa fa-download" aria-hidden="true"></i></a>
+                </br>
+            <i class="fa fa-dot-circle-o" aria-hidden="true" style="color:black;"></i><a href="/lastNed/Tittelside.docx"> Tittelside for bachelorprosjekter <i class="fa fa-download" aria-hidden="true"></i></a>
         </div>
     </div>
 @endsection
