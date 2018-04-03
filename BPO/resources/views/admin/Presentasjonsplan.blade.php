d8ace95351dbe53f1779346f2a10ec731e08bf67
Ivo <s305516@stud.hioa.no>
2018-04-03 03:09:12 +0200

    la inn litt mere på presentasjonsplanen for å få oversikt som administrator

--- BPO/resources/views/admin/Presentasjonsplan.blade.php
+++ BPO/resources/views/admin/Presentasjonsplan.blade.php
@@ -27,27 +27,59 @@
             {!! Form::close() !!}
             </br>
         </div>
-        <br>
-        <div style="max-width:17em;"class="no-padding-left col">
-            {!! Form::open(['action' => 'PresentasjonController@store', 'method' => 'POST', 'class' => 'row form-group']) !!}
-                <h4 class="no-padding-left col-12">Dag:</h4>
-                <input type="date" class="col-8 form-control" name="dates" required>
-                <input type="time" class="col-4 form-control" name="time" required>
-                <select required class="custom-select form-control margin-fix-top" name="room">
-                    <option disabled value="" selected >Velg Rom</option>
-                    @foreach($rooms as $room)
-                        <option value={{$room->room}}>{{$room->room}}</option>
-                    @endforeach
-                </select>
-                <select required class="custom-select form-control margin-fix-top" name={{"sensor"}}>
-                    <option disabled value="" selected >Sensor</option>
-                    @foreach($supervisors as $supervisor)
-                        <option value={{$supervisor->email}}>{{$supervisor->firstname." ".$supervisor->lastname}}</option>
-                    @endforeach
-                </select>
-                <input class="col-12 form-control margin-fix-top" type="number" name="perr_dag" min="1" placeholder="Antall presentasjoner perr dag" class="form-control margin-fix-bottom" value="11" required>
-                {{Form::submit('Oppdater', ['class'=>'btn btn-success col-12 margin-fix-top'])}}
+        <div style="margin-top: 5em;"></div>
+        <div class="row">
+            {!! Form::open(['action' => 'PresentasjonController@store', 'method' => 'POST', 'class' => 'col-4 form-group']) !!}
+                <div class="row">
+                    <h4 class="no-padding-left col-12">Dag:</h4>
+                    <input type="date" class="col-8 form-control form-control-lg" name="dates" required>
+                    <input type="time" class="col-4 form-control form-control-lg" name="time" required>
+                    <select required class="custom-select form-control form-control-lg margin-fix-top" name="room">
+                        <option disabled value="" selected >Velg Rom</option>
+                        @foreach($rooms as $room)
+                            <option value={{$room->room}}>{{$room->room}}</option>
+                        @endforeach
+                    </select>
+                    <select required class="custom-select form-control form-control-lg margin-fix-top" name={{"sensor"}}>
+                        <option disabled value="" selected >Sensor</option>
+                        @foreach($supervisors as $supervisor)
+                            <option value={{$supervisor->email}}>{{$supervisor->firstname." ".$supervisor->lastname}}</option>
+                        @endforeach
+                    </select>
+                    <input class="col-12 form-control form-control-lg margin-fix-top" type="number" name="perr_dag" min="1" placeholder="Antall presentasjoner perr dag" class="form-control margin-fix-bottom" value="11" required>
+                    {{Form::submit('Oppdater', ['class'=>'btn btn-success col-12 margin-fix-top'])}}
+                </div>
             {!! Form::close() !!}
+            <div class="col-4 offset-4 no-padding-right">
+                <h4>Grupper som ikke er registrerte</h4>
+                <div class="jumbotron width-fill" style="padding:0 !important">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Gruppe</th>
+                                    <th>År</th>
+                                    <th>Leder</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($groups as $group)
+                                <tr>
+                                    <th scope="row">{{$group->group_number}}</th>
+                                    <td>{{$group->year}}</td>
+                                    <td>{{$group->leader}}</td>
+                                </tr>
+                                @endforeach
+                                @if(!$groups)
+                                    <tr>
+                                        <td>alle gruppene ligger i presentasjonsplanen</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table> 
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
