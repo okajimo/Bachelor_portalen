ac06d079eca25c41182fb532277e4a6391a2763b
Stian <stian__95@hotmail.com>
2018-05-01 20:29:44 +0200

    validering av laste opp prosjektforslag

--- BPO/app/Http/Controllers/Admin/ProsjektforslagController.php
+++ BPO/app/Http/Controllers/Admin/ProsjektforslagController.php
@@ -29,7 +29,7 @@ class ProsjektforslagController extends Controller
     {
         $this->validate($request, [
             'dok' => 'required|mimes:pdf|max:1999',
-            'file_name' => 'required'
+            'file_name' => 'required|regex:/(^[A-Za-z0-9 ÅØÆåøæ-.]+$)/'
         ]);
 
         $upload = \UploadHelper::instance()->upload($request);
