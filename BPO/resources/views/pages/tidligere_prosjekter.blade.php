494539731b54d1029d9d894ab492c35430e68ec1
Martin <s305454@stud.hioa.no>
2018-06-13 02:07:02 +0200

    fix

--- BPO/resources/views/pages/tidligere_prosjekter.blade.php
+++ BPO/resources/views/pages/tidligere_prosjekter.blade.php
@@ -2,22 +2,34 @@
 @section('content')
     <div class="jumbotron">
         <div class="container">
-                Velg året du vil se tidligere prosjekter fra.
-                <table class='table'>
-                    <tr>
-                        <td>
-                            <?php
-                            $rapport = Storage::exists('/public/filer/tidligere_prosjekter_sluttrapport');
-                            if($rapport)
+        <h3>Velg året du vil se tidligere prosjekter fra.</h3>
+            <table>
+                <tr>
+                    <td>
+                        <?php
+                        $rapport = Storage::exists('/public/filer/tidligere_prosjekter_sluttrapport');
+                        if($rapport)
+                        {
+                            $file = scandir('storage/filer/tidligere_prosjekter_sluttrapport/'); 
+                            foreach($file as $f)
                             {
-                                $file = scandir('storage/filer/tidligere_prosjekter_sluttrapport/'); 
-                                foreach($file as $f)
+                                if($f !== '.' && $f !== '..')
                                 {
-                                   if($f !== '.' && $f !== '..')
-                                   {
-                                        $mounth = date('m');
-                                        $dato1 = date('Y');
-                                        if($f < $dato1)
+                                    $mounth = date('m');
+                                    $dato1 = date('Y');
+                                    if($f < $dato1)
+                                    {
+                                        ?>  {!! Form::open(['action' => 'Tidligere_prosjekterController@showTidligereProsjekter', 'method' => 'POST']) !!}  
+                                                {{Form::hidden('year', $f)}}
+                                                <div class="form-group">
+                                                </div>
+                                                {{Form::submit($f,['class'=>'btn btn-primary', 'name' => 'sub'])}}   
+                                            {!! Form::close() !!}
+                                        <?php
+                                    }
+                                    elseif($f == $dato1)
+                                    {
+                                        if($mounth > 07)
                                         {
                                             ?>  {!! Form::open(['action' => 'Tidligere_prosjekterController@showTidligereProsjekter', 'method' => 'POST']) !!}  
                                                     {{Form::hidden('year', $f)}}
@@ -27,48 +39,30 @@
                                                 {!! Form::close() !!}
                                             <?php
                                         }
-                                        elseif($f == $dato1)
-                                        {
-                                            if($mounth > 07)
-                                            {
-                                                ?>  {!! Form::open(['action' => 'Tidligere_prosjekterController@showTidligereProsjekter', 'method' => 'POST']) !!}  
-                                                        {{Form::hidden('year', $f)}}
-                                                        <div class="form-group">
-                                                        </div>
-                                                        {{Form::submit($f,['class'=>'btn btn-primary', 'name' => 'sub'])}}   
-                                                    {!! Form::close() !!}
-                                                <?php
-                                            }
-                                        }
-                                   }
+                                    }
                                 }
                             }
-                            else 
-                            {
-                                echo "Ingen tidligere prosjekter tilgjengelig.";    
-                            }
-                           ?>
-                        </td>
-                        <td>
-                            <?php echo Session::get('hvis') ?>
-                        </td>
-                    </tr>
-                </table>
-        </div>
-    </div>
-    <div class="jumbotron">
-        <div class="container">
-            Tidligere prosjekter fra den gamle nettsiden. </br>
-
-            <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2017/' class='blaaLink'>2017</a><br/>
-            <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2016/' class='blaaLink'>2016</a><br/>
-            <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2015/' class='blaaLink'>2015</a><br/>
-            <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2014/' class='blaaLink'>2014</a><br/>
-            <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2013/' class='blaaLink'>2013</a><br/>
-            <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2012/" class="blaaLink">2012</a><br/>
-            <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2011/" class="blaaLink">2011</a><br/>
-            <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2010/" class="blaaLink">2010</a><br/>
-            <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2009/" class="blaaLink">2009</a><br/>
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <?php echo Session::get('hvis') ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2017/' class='blaaLink'><b>2017</b></a><br/>
+                        <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2016/' class='blaaLink'><b>2016</b></a><br/>
+                        <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2015/' class='blaaLink'><b>2015</b></a><br/>
+                        <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2014/' class='blaaLink'><b>2014</b></a><br/>
+                        <a href='http://www.cs.hioa.no/data/bachelorprosjekt/2013/' class='blaaLink'><b>2013</b></a><br/>
+                        <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2012/" class='blaaLink'><b>2012</b></a><br/>
+                        <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2011/" class='blaaLink'><b>2011</b></a><br/>
+                        <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2010/" class='blaaLink'><b>2010</b></a><br/>
+                        <a href="http://www.cs.hioa.no/data/bachelorprosjekt/2009/" class='blaaLink'><b>2009</b></a><br/>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 @endsection
